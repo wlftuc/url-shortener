5bc3327a1532bf9f8ba141fba6cba61c50b4d413
wlftuc <yshsbhat@outlook.com>
2022-06-21 00:51:09 +0530

    Rush B!

--- pages/api/shorten.ts
+++ pages/api/shorten.ts
@@ -1,7 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import { prisma } from "../../lib/prisma"; // TYPE SAFE WOHOOO!
+
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const { url } = req.body;
+
+  
 
   res.status(200).json({ url: url });
 };
