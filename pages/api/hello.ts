7747b1cf62b08c1137c62d72bb707fdb58542f00
wlftuc <yshsbhat@outlook.com>
2022-06-28 19:50:03 +0530

    test the `cause` property of `Error`

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -7,8 +7,13 @@ import { Hash } from "../../lib/secure/secure";
 let { HASH_TOKEN, SECURE_AES_IV } = process.env;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({
-    message: "Hello World",
-    wrong: true,
-  });
+  try {
+    //@ts-expect-error TEST
+    [1, 5, 5, 5, 2].split();
+    return res.status(200).json({
+      works: true,
+    });
+  } catch (err) {
+    throw new Error(err, { cause: err });
+  }
 }
