5a050a84f77e7304070bf13760b4cae1d455414e
wlftuc <yshsbhat@outlook.com>
2022-06-28 19:45:42 +0530

    remove unused code

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -7,22 +7,6 @@ import { Hash } from "../../lib/secure/secure";
 let { HASH_TOKEN, SECURE_AES_IV } = process.env;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const hash = new Hash(HASH_TOKEN);
-
-  const e = hash.encrypt("https://helloworld.com");
-  const d = hash.decrypt(e);
-
-  const hashPass = hash.hashPassword("Hello world!").toString();
-
-  console.log(hashPass);
-
-  if (hashPass === hash.hashPassword("Hello world!").toString())
-    console.log(true);
-
-  console.log("here");
-  console.log(e);
-  console.log(d);
-
   return res.status(200).json({
     message: "Hello World",
     wrong: true,
